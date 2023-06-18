6264e87596c8d95ecec93166fc6d991df3f100b6
Brent Turner <brentturner7@gmail.com>
2023-06-18 19:17:54 -0400

    Add Scale Axis for Time in minutes so user can have more data points, added start values for dates, and changed from 3 series to 5

--- src/helper.js
+++ src/helper.js
@@ -12,19 +12,12 @@ function getDaysBetweenDates(date1, date2) {
     return days;
 }
 
-function getHoursBetweenDates(date1, date2) {
-    // Convert the date strings to Date objects
-    const startDate = new Date(date1);
-    const endDate = new Date(date2);
-
-    // Calculate the time difference in milliseconds
-    const timeDifference = endDate.getTime() - startDate.getTime();
-
-    // Calculate the number of hours
-    const hours = Math.floor(timeDifference / (1000 * 60 * 60));
-
-    return hours;
+function getMinutesBetweenDates(date1, date2) {
+    const diffInMilliseconds = Math.abs(date2 - date1);
+    const minutes = Math.floor(diffInMilliseconds / (1000 * 60));
+    return minutes;
 }
+  
 
 var currentIndexClassName = 'current-index';
 
